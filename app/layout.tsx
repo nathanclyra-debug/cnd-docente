95eb28b793e677e1ba1ad45e4da2639a47bd8d60
nathanclyra-debug <nathanclyra@gmail.com>
2026-09-21 18:08:44 -0300

    feat: build CND platform

---
+++ app/layout.tsx
@@ -0,0 +1,1 @@
+import "./globals.css";export const metadata={title:"Carteira Nacional do Docente",description:"Exame Nacional de Proficiência na Docência"};export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="pt-BR"><body>{children}</body></html>}
